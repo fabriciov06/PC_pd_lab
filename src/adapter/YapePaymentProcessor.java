349ba2f8432db457ad36187aadb921a66a31d8f8
Fabricio Vera <sebastianvt2006@gmail.com>
2026-05-23 17:53:08 -0500

    feat: Agregue el procesador de pagos con yape

--- src/adapter/YapePaymentProcessor.java
+++ src/adapter/YapePaymentProcessor.java
@@ -1,4 +1,8 @@
 package adapter;
 
-public class YapePaymentProcessor {
+public class YapePaymentProcessor implements PaymentProcessor{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Pago procesado con Yape por: S/ " + amount);
+    }
 }
