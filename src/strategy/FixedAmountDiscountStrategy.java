8f0512f7c72e5f162fe29532c6f9fd2da3cb2190
Fabricio Vera <sebastianvt2006@gmail.com>
2026-05-23 17:24:20 -0500

    feat: Implemente la clase FixedAmountDiscountStrategy con su constructor junto al metodo applyDiscount para aplicar descuentos de monto fijo

--- src/strategy/FixedAmountDiscountStrategy.java
+++ src/strategy/FixedAmountDiscountStrategy.java
@@ -1,7 +1,14 @@
 package strategy;
 
-public class FixedAmountDiscountStrategy {
+public class FixedAmountDiscountStrategy implements DiscountStrategy {
+    private double discountAmount;
 
+    public FixedAmountDiscountStrategy(double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
 
+    @Override
+    public double applyDiscount(double total) {
+        return (total >= discountAmount) ? (total - discountAmount) : 0.0;
+    }
 }
-
