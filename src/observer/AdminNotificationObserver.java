c600d961def1e6b28cdf7deb7cdd6b082ebc87e2
Fabricio Vera <sebastianvt2006@gmail.com>
2026-05-23 18:07:29 -0500

    feat: Implemente AdminNotificationObserver para emitir alertas al administrador

--- src/observer/AdminNotificationObserver.java
+++ src/observer/AdminNotificationObserver.java
@@ -1,4 +1,8 @@
 package observer;
 
-public class AdminNotificationObserver {
+public class AdminNotificationObserver implements OrderObserver{
+    @Override
+    public void update(String message) {
+        System.out.println("Notificando al administrador...");
+    }
 }
