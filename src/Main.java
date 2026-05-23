9cd4f783f3a45ff422df8e99fdd427c2e2414862
Fabricio Vera <sebastianvt2006@gmail.com>
2026-05-23 18:37:00 -0500

    feat: Cree el flujo completo de la tienda virtual en la clase Main uniendo los tres patrones

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import model.Cart;
+import model.Product;
+import strategy.DiscountStrategy;
+import strategy.PercentageDiscountStrategy;
+import adapter.ExternalPayPalService;
+import adapter.PayPalAdapter;
+import adapter.PaymentProcessor;
+import observer.AdminNotificationObserver;
+import observer.EmailNotificationObserver;
+import observer.InventoryObserver;
+import service.OrderService;
 
-public class Main{
-    public static void main(String[] args){}
-    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public static void main(String[] args) {
+        Product p1 = new Product("Laptop Gamer", 3500.00);
+        Product p2 = new Product("Mouse Inalambrico", 150.00);
+
+        Cart cart = new Cart();
+        cart.addProduct(p1);
+        cart.addProduct(p2);
+
+        double total = cart.calculateTotal();
+
+        DiscountStrategy discount = new PercentageDiscountStrategy(10);
+        double finalTotal = discount.applyDiscount(total);
+
+        OrderService orderService = new OrderService();
+        orderService.addObserver(new EmailNotificationObserver());
+        orderService.addObserver(new InventoryObserver());
+        orderService.addObserver(new AdminNotificationObserver());
+
+        ExternalPayPalService externalPayPal = new ExternalPayPalService();
+        PaymentProcessor paymentProcessor = new PayPalAdapter(externalPayPal);
+
+        paymentProcessor.pay(finalTotal);
+        orderService.confirmOrder(finalTotal);
+    }
 }
