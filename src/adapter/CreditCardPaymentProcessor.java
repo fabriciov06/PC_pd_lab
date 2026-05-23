6a962f684154f551a9f9ed1e2d13ff3c8ae364bb
Fabricio Vera <sebastianvt2006@gmail.com>
2026-05-23 17:49:04 -0500

    feat: Agregue CreditCardPaymentProcessor como metodo de pago nativo y compatible

--- src/adapter/CreditCardPaymentProcessor.java
+++ src/adapter/CreditCardPaymentProcessor.java
@@ -1,4 +1,8 @@
 package adapter;
 
-public class CreditCardPaymentProcessor {
+public class CreditCardPaymentProcessor implements PaymentProcessor{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Pago procesado con Tarjeta de Crédito por: S/ " + amount);
+    }
 }
