dc72177f8ce796878331a040b2a0608987403b41
Fabricio Vera <sebastianvt2006@gmail.com>
2026-05-23 16:55:04 -0500

    feat: Añadi la lista de productos junto con los metodos addProducto y calculateTotal respectivos de la clase Cart junto con las librerias ArrayList, List

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,4 +1,21 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println("Producto agregado: " + product.getName());
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
 }
